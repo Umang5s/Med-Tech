568d6985e38e3264bb8fc745d70386b9020ed6e2
Umang5s <220130107104@gecg28.ac.in>
2025-02-28 11:59:38 +0530

    newone

--- Frontend/pages/Start.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Start = () => {
-  return (
-    <div className='bg-red-500'>Start</div>
-  );
-}
-
-export default Start;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,23 +1,10 @@
-// filepath: /C:/Users/Krish-Rudra/OneDrive/Desktop/krish MERN/UberClone/Frontend/src/main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import UserContext from '../context/UserContext.jsx';
-import DoctorContext from '../context/DoctorContext.jsx';
-// import SocketProvider from './context/SocketContext.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DoctorContext>
-      <UserContext>
-        {/* <SocketProvider> */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-        {/* </SocketProvider> */}
-      </UserContext>
-    </DoctorContext>
+    <App />
   </StrictMode>,
-);
+)

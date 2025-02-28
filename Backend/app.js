63ada61dbb26fbbe21aa1f3e2187f3146df5cbc3
Krish Parikh <115562197+krish-0510@users.noreply.github.com>
2025-02-28 11:20:07 +0530

    Implement messaging feature with routes, controller, service, and socket integration

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ const connectToDb = require('./db/db');
 //ALL ROUTES
 const userRoutes = require('./routes/user.routes');
 const doctorRoutes = require('./routes/doctor.routes');
+const messageRoutes = require('./routes/message.routes');
 
 connectToDb();
 
@@ -18,13 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
 app.use('/users', userRoutes);
 app.use('/doctors', doctorRoutes);
-
+app.use('/messages', messageRoutes);
 
 module.exports = app;
